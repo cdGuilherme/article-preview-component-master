ed8c6bc2a6b7478220c09609c2ad40ba0a465314
nanikore0 <167817674+cdGuilherme@users.noreply.github.com>
2024-12-18 12:49:11 -0300

    Updating attribution share style
    - added new svg for active state
    - change the color of the button depending if it's active or not

--- script/main.js
+++ script/main.js
@@ -1,4 +1,19 @@
 function toggleShare() {
+  // hides / unhides share toggle
   let shareComponent = document.getElementById("toggle");
   shareComponent.classList.toggle("hidden");
+
+  // get the current color of the toggle button element so we can
+  // change to the correct colors
+  var currentColor = document.getElementById("toggle-button").style.backgroundColor;
+
+  if (currentColor != "rgb(109, 127, 151)") {
+    // active state
+    document.getElementById("toggle-button").style.backgroundColor = "#6d7f97";
+    document.getElementById("toggle-button--image").src="/images/icon-share-active.svg";
+  } else {
+    // not active
+    document.getElementById("toggle-button").style.backgroundColor = "#ecf2f8";
+    document.getElementById("toggle-button--image").src="/images/icon-share.svg";
+  }
 }
